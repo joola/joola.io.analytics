db629942d60725cb673533d0f3d4c981c7a01e73
Itay Weinberger <itay@joo.la>
2013-07-12 20:15:40 +0300

    packaging

---
+++ bin/joola-analytics.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set my_command=node
+set my_params="%~dp0\joola-analytics" %*
+
+    %my_command% %my_params%
+
+
---
+++ config/joola.analytics.sample.js
@@ -0,0 +1,24 @@
+var configData = {};
+
+/////////////////////////
+// General Information //
+/////////////////////////
+
+configData.general = {
+    serverName: "joola-analytics",
+    id: "joola-analytics-server",
+    serverToken: "joola-analytics-server",
+    flatFileDirectory: __dirname + "/public/",
+    developmentMode: true,
+    port: 80
+};
+
+configData.cache = {
+
+};
+
+configData.joolaServer = {
+    ip: "127.0.0.1",
+    port: "8080",
+    "authToken": "d2ead440-eb12-11e2-91e2-0800200c9a66"
+};
