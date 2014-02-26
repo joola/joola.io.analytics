3b3e6aa5b762625d292a88050e2de5ebf11a1ab0
orweinberger <orweinberger@gmail.com>
2014-02-26 12:19:27 +0200

    #25 Clean slate, new express template

--- bin/joola.io.analytics.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set my_command=node
-set my_params="%~dp0\..\joola.io.analytics" %*
-
-    %my_command% %my_params%
--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,8 @@
-var
-  http = require('http'),
-  login = require('./login');
 
-exports.index = function (req, res) {
-  /*if (!req.session || !req.session.token) {
-    login.checkLoginNeeded(function (needed) {
-      if (needed) {
-        res.redirect('/login');
-      }
-      else {
-        res.redirect('/homepage');
-      }
-    });
-  }
-  else*/
-    res.render('index', { joolaioToken: req.session.token, sampleData: joola.config.get('server:sampleData'), user: req.session.displayName  });
-};
+/*
+ * GET home page.
+ */
 
-exports.index2 = function (req, res) {
-  /*if (!req.session || !req.session.token) {
-    login.checkLoginNeeded(function (needed) {
-      if (needed) {
-        res.redirect('/login');
-      }
-      else {
-        req.session.token = '123';
-        console.log('test');
-        res.render('index', { joolaioToken: req.session.token, sampleData: joola.config.get('server:sampleData') });
-        console.log('test2');
-      }
-    });
-  }
-  else*/
-    res.render('index', {joolaioToken: req.session.token, sampleData: joola.config.get('server:sampleData') , user: req.session.displayName});
-};
-
-exports.homepage = function (req, res) {
-  res.render('homepage');
-};
-
-exports.sdktest = function (req, res) {
-  res.render('sdktest');
-};
-
-exports.test = function (req, res) {
-  if (!req.session || !req.session.token) {
-    login.checkLoginNeeded(function (needed) {
-      if (needed) {
-        res.redirect('/login');
-      }
-      else {
-        req.session.token = '123';
-        console.log('test');
-        res.render('index', { joolaioToken: req.session.token, sampleData: joola.config.get('server:sampleData') });
-        console.log('test2');
-      }
-    });
-  }
-  else
-    res.render('test', {joolaioToken: req.session.token, sampleData: joola.config.get('server:sampleData') });
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
 };
